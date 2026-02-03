066a296cee12c3200b9586eaa7f7ace690763e26
Ygor Lazaro <ygor@ygorlazaro.com>
2026-02-03 04:26:21 -0300

    Removing logo from company

---
+++ Fenicia.Auth/Migrations/20260203072541_CompanyLogoRemove.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Fenicia.Auth.Migrations
+{
+    /// <inheritdoc />
+    public partial class CompanyLogoRemove : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "logo",
+                table: "companies");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "logo",
+                table: "companies",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: true);
+        }
+    }
+}
