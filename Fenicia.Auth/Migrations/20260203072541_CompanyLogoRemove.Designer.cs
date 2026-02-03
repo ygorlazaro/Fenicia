a26b87e11247644950721fa107f81ffdfacb717e
Ygor Lazaro <ygor@ygorlazaro.com>
2026-02-03 04:26:21 -0300

    Removing logo from company

--- Fenicia.Auth/Migrations/20260203072541_CompanyLogoRemove.Designer.cs
+++ Fenicia.Auth/Migrations/20260203072541_CompanyLogoRemove.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Fenicia.Common.Database.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Fenicia.Auth.Migrations
 {
     [DbContext(typeof(AuthContext))]
-    partial class AuthContextModelSnapshot : ModelSnapshot
+    [Migration("20260203072541_CompanyLogoRemove")]
+    partial class CompanyLogoRemove
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
