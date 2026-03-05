e3b862bbdef2bae6cb182766a02beed04995e72a
Ygor Lazaro <ygor@ygorlazaro.com>
2026-03-04 21:11:29 -0300

    Front: removing light mode

--- fenicia-web/src/components/AppHeader.tsx
+++ fenicia-web/src/components/AppHeader.tsx
@@ -2,28 +2,15 @@ import React, { useEffect, useRef, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
 import {
-  CContainer,
-  CDropdown,
-  CDropdownItem,
-  CDropdownMenu,
-  CDropdownToggle,
-  CHeader,
-  CHeaderNav,
-  CHeaderToggler,
-  CNavLink,
-  CNavItem,
-  useColorModes,
+    CContainer,
+    CHeader,
+    CHeaderNav,
+    CHeaderToggler,
+    CNavLink,
+    CNavItem, useColorModes,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
-import {
-  cilBell,
-  cilContrast,
-  cilEnvelopeOpen,
-  cilList,
-  cilMenu,
-  cilMoon,
-  cilSun,
-} from '@coreui/icons'
+import {cilContrast, cilMenu, cilMoon, cilSun} from '@coreui/icons'
 import { useTranslation } from 'react-i18next'
 
 import { AppBreadcrumb } from './index'
@@ -38,11 +25,9 @@ const companyClient = new AuthCompanyClient("http://localhost:5144")
 const AppHeader = () => {
   const headerRef = useRef()
   const { t } = useTranslation()
-  const { colorMode, setColorMode } = useColorModes('fenicia-gato-ninja-theme')
-
   const dispatch = useDispatch()
   const sidebarShow = useSelector((state) => state.sidebarShow)
-  
+
   // Company selection modal state
   const [showCompanyModal, setShowCompanyModal] = useState(false)
   const [companies, setCompanies] = useState([])
@@ -92,8 +77,7 @@ const AppHeader = () => {
   // Translate menu items
   const menuItems = [
     { path: '/dashboard', label: t('menu.dashboard') },
-    { path: '/auth/user', label: t('menu.employees') },
-    { path: '#', label: t('common.actions') }
+    { path: '/profile', label: t('menu.profile') }
   ]
 
   return (
@@ -115,49 +99,6 @@ const AppHeader = () => {
           ))}
         </CHeaderNav>
         <CHeaderNav>
-          <CDropdown variant="nav-item" placement="bottom-end">
-            <CDropdownToggle caret={false}>
-              {colorMode === 'dark' ? (
-                <CIcon icon={cilMoon} size="lg" />
-              ) : colorMode === 'auto' ? (
-                <CIcon icon={cilContrast} size="lg" />
-              ) : (
-                <CIcon icon={cilSun} size="lg" />
-              )}
-            </CDropdownToggle>
-            <CDropdownMenu>
-              <CDropdownItem
-                active={colorMode === 'light'}
-                className="d-flex align-items-center"
-                as="button"
-                type="button"
-                onClick={() => setColorMode('light')}
-              >
-                <CIcon className="me-2" icon={cilSun} size="lg" /> {t('common.light')}
-              </CDropdownItem>
-              <CDropdownItem
-                active={colorMode === 'dark'}
-                className="d-flex align-items-center"
-                as="button"
-                type="button"
-                onClick={() => setColorMode('dark')}
-              >
-                <CIcon className="me-2" icon={cilMoon} size="lg" /> {t('common.dark')}
-              </CDropdownItem>
-              <CDropdownItem
-                active={colorMode === 'auto'}
-                className="d-flex align-items-center"
-                as="button"
-                type="button"
-                onClick={() => setColorMode('auto')}
-              >
-                <CIcon className="me-2" icon={cilContrast} size="lg" /> {t('common.auto')}
-              </CDropdownItem>
-            </CDropdownMenu>
-          </CDropdown>
-          <li className="nav-item py-1">
-            <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
-          </li>
           <LanguageSelector />
           <li className="nav-item py-1">
             <div className="vr h-100 mx-2 text-body text-opacity-75"></div>
