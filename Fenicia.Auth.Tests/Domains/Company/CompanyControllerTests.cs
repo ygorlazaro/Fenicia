a844c21f32df31045c2df04217de0a29a59797d8
Ygor Lazaro <ygor@ygorlazaro.com>
2026-04-22 14:57:53 -0300

    Fenicia: cleaning models

--- Fenicia.Auth.Tests/Domains/Company/CompanyControllerTests.cs
+++ Fenicia.Auth.Tests/Domains/Company/CompanyControllerTests.cs
@@ -11,7 +11,6 @@ using Fenicia.Common;
 using Fenicia.Common.API;
 using Fenicia.Common.Data.Contexts;
 using Fenicia.Common.Data.Models.Auth;
-using Fenicia.Common.Exceptions;
 using Fenicia.Common.Tests;
 
 using Microsoft.AspNetCore.Authorization;
