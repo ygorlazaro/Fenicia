b130b334639d18080f22a2126299555706e1e53b
Ygor Lazaro <ygor@ygorlazaro.com>
2026-04-23 23:21:56 -0300

    front: fix

--- fenicia-web/src/routes.tsx
+++ fenicia-web/src/routes.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 const AuthLogin = React.lazy(() => import("./views/auth/login"));
 const AuthRegister = React.lazy(() => import("./views/auth/register"));
 const AuthCompany = React.lazy(() => import("./views/auth/company"));
-const AuthUser = React.lazy(() => import("./views/auth/user"));
 const ForgotPassword = React.lazy(() => import("./views/auth/forgot-password"));
 const ResetPassword = React.lazy(() => import("./views/auth/reset-password"));
 const Profile = React.lazy(() => import("./template/profile"));
@@ -125,7 +124,6 @@ const routes = [
   { path: '/auth/login', name: 'Login', element: AuthLogin },
   { path: '/auth/register', name: 'Register', element: AuthRegister },
   { path: '/auth/company', name: 'Company Selection', element: AuthCompany },
-  { path: '/auth/user', name: 'User Management', element: AuthUser },
   { path: '/auth/forgot-password', name: 'Forgot Password', element: ForgotPassword },
   { path: '/auth/reset-password', name: 'Reset Password', element: ResetPassword },
   { path: '/profile', name: 'Profile', element: Profile },
