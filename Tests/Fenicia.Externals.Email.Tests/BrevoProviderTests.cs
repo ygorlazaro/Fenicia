067aa7ad0abc5372ac1cdfa4c19ea0ad05346c57
Ygor Lazaro <ygor@ygorlazaro.com>
2026-08-29 09:45:55 -0300

    Add Fenicia.Externals.Email.Tests project

---
+++ Tests/Fenicia.Externals.Email.Tests/BrevoProviderTests.cs
@@ -0,0 +1,37 @@
+using Fenicia.Common.Enums.External;
+using Fenicia.Externals.Email;
+
+using FluentAssertions;
+
+namespace Fenicia.Externals.Email.Tests;
+
+public class BrevoProviderTests
+{
+    [Fact]
+    public void Constructor_ShouldCreateInstance()
+    {
+        var provider = new BrevoProvider();
+
+        provider.Should().NotBeNull();
+    }
+
+    [Fact]
+    public void Send_ShouldReadApiKeyFromEnvironment()
+    {
+        var provider = new BrevoProvider();
+        Action act = () => provider.Send(EmailTemplate.ForgotPassword, "test@example.com", "Test", null);
+
+        act.Should().Throw<Exception>();
+    }
+
+    [Fact]
+    public void Send_ShouldUseTemplateIdFromEnum()
+    {
+        var provider = new BrevoProvider();
+
+        var template = EmailTemplate.ForgotPassword;
+        var templateId = (int)template;
+
+        templateId.Should().Be(1);
+    }
+}
