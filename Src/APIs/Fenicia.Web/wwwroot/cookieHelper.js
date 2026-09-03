dbd86d653537819a3e343d450f92876743a727a9
Ygor Lazaro <ygor@ygorlazaro.com>
2026-09-03 09:59:30 -0300

    fix: remove forceLoad to stabilize circuit and add layout auth debug logs

--- Src/APIs/Fenicia.Web/wwwroot/cookieHelper.js
+++ Src/APIs/Fenicia.Web/wwwroot/cookieHelper.js
@@ -1,5 +1,6 @@
 window.cookieHelper = {
     set: function (name, value, hours) {
+        console.log(`[cookieHelper] Setting cookie: ${name}`);
         var expires = "";
         if (hours) {
             var date = new Date();
@@ -14,11 +15,18 @@ window.cookieHelper = {
         for (var i = 0; i < ca.length; i++) {
             var c = ca[i];
             while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
+            if (c.indexOf(nameEQ) === 0) {
+                var val = decodeURIComponent(c.substring(nameEQ.length, c.length));
+                console.log(`[cookieHelper] Found cookie ${name}: ${val.substring(0, 20)}...`);
+                return val;
+            }
         }
+        console.log(`[cookieHelper] Cookie ${name} not found`);
         return null;
     },
     remove: function (name) {
         document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
     }
 };
+
+console.log("[cookieHelper] Loaded successfully");
