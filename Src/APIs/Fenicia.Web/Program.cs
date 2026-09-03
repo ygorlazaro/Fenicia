e68f8cf81b8b485f9516e0c28b2f296dc4d16192
Ygor Lazaro <ygor@ygorlazaro.com>
2026-09-03 08:18:12 -0300

    chore: remove HTTPS for local development in frontend

--- Src/APIs/Fenicia.Web/Program.cs
+++ Src/APIs/Fenicia.Web/Program.cs
@@ -24,8 +24,6 @@ if (!app.Environment.IsDevelopment())
 
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 
-app.UseHttpsRedirection();
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
