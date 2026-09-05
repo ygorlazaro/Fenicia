6026dcb18295c8519da6c0d30b5f6e297719599b
Ygor Lazaro <ygor@ygorlazaro.com>
2026-09-05 03:12:08 -0300

    fix(socialnetwork): register missing repositories in DI
    The SocialNetwork module's services and controllers were never runnable
    because the IProfileService, IBlockRepository, IFriendshipRepository and
    all the concrete repositories (Feed, Like, Comment, Attachment, Share,
    Report) were missing from the DI container. Implementations for the
    Block and Friendship interfaces were also missing.
    
    - Implement BlockRepository and FriendshipRepository for their existing
      interfaces (interfaces were defined in BlockRepository.cs /
      FriendshipRepository.cs but no class implemented them)
    - Register IBlockRepository, IFriendshipRepository, and all concrete
      repositories in Program.cs

--- Src/APIs/Fenicia.Module.SocialNetwork/Domains/Block/BlockRepository.cs
+++ Src/APIs/Fenicia.Module.SocialNetwork/Domains/Block/BlockRepository.cs
@@ -1,9 +1,16 @@
+using Fenicia.Common.Data.Contexts;
 using Fenicia.Common.Data.Models.SocialNetwork;
 using Fenicia.Common.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace Fenicia.Module.SocialNetwork.Domains.Block;
 
 public interface IBlockRepository : IRepository<BlockModel>
 {
     new IQueryable<BlockModel> Query();
+}
+
+public class BlockRepository(DefaultContext context) : Repository<BlockModel>(context), IBlockRepository
+{
+    public new IQueryable<BlockModel> Query() => DbSet;
 }
--- Src/APIs/Fenicia.Module.SocialNetwork/Program.cs
+++ Src/APIs/Fenicia.Module.SocialNetwork/Program.cs
@@ -2,8 +2,16 @@ using Fenicia.Common.API;
 using Fenicia.Common.API.Startup;
 using Fenicia.Common.Data;
 using Fenicia.Common.Data.Contexts;
+using Fenicia.Module.SocialNetwork.Domains.Attachment;
+using Fenicia.Module.SocialNetwork.Domains.Block;
+using Fenicia.Module.SocialNetwork.Domains.Comment;
+using Fenicia.Module.SocialNetwork.Domains.Feed;
+using Fenicia.Module.SocialNetwork.Domains.Friendship;
+using Fenicia.Module.SocialNetwork.Domains.Like;
 using Fenicia.Module.SocialNetwork.Domains.Profile;
 using Fenicia.Module.SocialNetwork.Domains.Profile.Interfaces;
+using Fenicia.Module.SocialNetwork.Domains.Report;
+using Fenicia.Module.SocialNetwork.Domains.Share;
 
 namespace Fenicia.Module.SocialNetwork;
 
@@ -21,6 +29,14 @@ public class Program
                 builder.Services.AddHttpContextAccessor();
                 builder.Services.AddScoped<IProfileRepository, ProfileRepository>();
                 builder.Services.AddScoped<IProfileService, ProfileService>();
+                builder.Services.AddScoped<FeedRepository>();
+                builder.Services.AddScoped<LikeRepository>();
+                builder.Services.AddScoped<CommentRepository>();
+                builder.Services.AddScoped<AttachmentRepository>();
+                builder.Services.AddScoped<ShareRepository>();
+                builder.Services.AddScoped<ReportRepository>();
+                builder.Services.AddScoped<IBlockRepository, BlockRepository>();
+                builder.Services.AddScoped<IFriendshipRepository, FriendshipRepository>();
             }).AddFeniciaDbContext<DefaultContext>(configuration, "Fenicia.Auth", "Auth");
 
         var app = builder.Build();
