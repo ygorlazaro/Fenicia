b39583bd1fa5d840a14a1617fc7fdf76417501fc
Ygor Lazaro <ygor@ygorlazaro.com>
2026-09-06 02:45:07 -0300

    feat: aplicar validacao CnpjAttribute no backend do register

--- Src/APIs/Fenicia.Auth/Domains/User/DTOs/CreateNewUserCompanyCommand.cs
+++ Src/APIs/Fenicia.Auth/Domains/User/DTOs/CreateNewUserCompanyCommand.cs
@@ -1,7 +1,8 @@
 using System.ComponentModel.DataAnnotations;
+using Fenicia.Common.Validations;
 
 namespace Fenicia.Auth.Domains.User.DTOs;
 
 public record CreateNewUserCompanyCommand(
-    [Required] [MaxLength(200)] string Cnpj,
+    [Required] [MaxLength(200)] [Cnpj] string Cnpj,
     [Required] [MaxLength(200)] string Name);
