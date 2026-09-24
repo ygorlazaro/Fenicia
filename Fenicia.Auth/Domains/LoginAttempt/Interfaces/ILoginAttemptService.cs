5509217dddf626227c24cd00ab6145304265e283
Ygor Lazaro <ygor@ygorlazaro.com>
2026-09-24 00:32:25 -0300

    Auth: Pattern at Login Attempt

--- Fenicia.Auth/Domains/LoginAttempt/Interfaces/ILoginAttemptService.cs
+++ Fenicia.Auth/Domains/LoginAttempt/Interfaces/ILoginAttemptService.cs
@@ -1,10 +1,27 @@
 namespace Fenicia.Auth.Domains.LoginAttempt.Interfaces;
 
+/// <summary>
+/// Service interface for managing login attempts, including tracking the number of attempts, incrementing attempts, and resetting attempts for a given email address.
+/// </summary>
 public interface ILoginAttemptService
 {
+    /// <summary>
+    /// Gets the number of login attempts for a specific email address.
+    /// </summary>
+    /// <param name="email">The email address.</param>
+    /// <returns>The number of login attempts.</returns>
     int GetAttempts(string email);
 
+    /// <summary>
+    /// Increments the number of login attempts for a specific email address.
+    /// </summary>
+    /// <param name="email">The email address.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     Task IncrementAsync(string email);
 
+    /// <summary>
+    /// Resets the number of login attempts for a specific email address.
+    /// </summary>
+    /// <param name="email">The email address.</param>
     void Reset(string email);
 }
--- Fenicia.Auth/Domains/LoginAttempt/LoginAttemptService.cs
+++ Fenicia.Auth/Domains/LoginAttempt/LoginAttemptService.cs
@@ -3,13 +3,32 @@ using StackExchange.Redis;
 
 namespace Fenicia.Auth.Domains.LoginAttempt;
 
+/// <summary>
+/// Represents a service for managing login attempts.
+/// </summary>
+/// <param name="redis">The Redis connection multiplexer.</param>
 public class LoginAttemptService(IConnectionMultiplexer redis) : ILoginAttemptService
 {
+    /// <summary>
+    /// The expiration time for login attempts in minutes.
+    /// </summary>
     private const int _expirationMinutes = 15;
+
+    /// <summary>
+    /// The prefix for the Redis key used to store login attempt information.
+    /// </summary>
     private const string _keyPrefix = "login-attempt:";
 
+    /// <summary>
+    /// The Redis database instance used for storing login attempt information.
+    /// </summary>
     private readonly IDatabase _redisDb = redis.GetDatabase();
 
+    /// <summary>
+    /// Gets the number of login attempts for a specific email address.
+    /// </summary>
+    /// <param name="email">The email address.</param>
+    /// <returns>The number of login attempts.</returns>
     public int GetAttempts(string email)
     {
         var key = GetKey(email);
@@ -18,6 +37,11 @@ public class LoginAttemptService(IConnectionMultiplexer redis) : ILoginAttemptSe
         return value.HasValue ? (int)value : 0;
     }
 
+    /// <summary>
+    /// Increments the number of login attempts for a specific email address.
+    /// </summary>
+    /// <param name="email">The email address.</param>
+    /// <returns>A task representing the asynchronous operation.</returns>
     public async Task IncrementAsync(string email)
     {
         var key = GetKey(email);
@@ -33,11 +57,20 @@ public class LoginAttemptService(IConnectionMultiplexer redis) : ILoginAttemptSe
             CommandFlags.None);
     }
 
+    /// <summary>
+    /// Resets the number of login attempts for a specific email address.
+    /// </summary>
+    /// <param name="email">The email address.</param>
     public void Reset(string email)
     {
         _redisDb.KeyDelete(GetKey(email));
     }
 
+    /// <summary>
+    /// Generates the Redis key for storing login attempt information based on the provided email address.
+    /// </summary>
+    /// <param name="email">The email address.</param>
+    /// <returns>The Redis key.</returns>
     private static string GetKey(string email)
     {
         ArgumentNullException.ThrowIfNull(email);
