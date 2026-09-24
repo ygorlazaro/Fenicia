e876921d603a6b599de307bc5a77e1188580ff90
Ygor Lazaro <ygor@ygorlazaro.com>
2026-09-24 11:31:34 -0300

    Auth: Pattern at Secutiry

--- Fenicia.Auth/Domains/Security/Interfaces/ISecurityService.cs
+++ Fenicia.Auth/Domains/Security/Interfaces/ISecurityService.cs
@@ -1,8 +1,22 @@
 namespace Fenicia.Auth.Domains.Security.Interfaces;
 
+/// <summary>
+/// Represents a service for handling security-related operations, such as hashing and verifying passwords.
+/// </summary>
 public interface ISecurityService
 {
+    /// <summary>
+    /// Hashes the provided original string (e.g., a password) and returns the hashed value.
+    /// </summary>
+    /// <param name="original">The original string to hash.</param>
+    /// <returns>The hashed string.</returns>
     string Hash(string original);
 
+    /// <summary>
+    /// Verifies that the provided password matches the hashed password.
+    /// </summary>
+    /// <param name="password">The password to verify.</param>
+    /// <param name="hashedPassword">The hashed password to compare against.</param>
+    /// <returns>true if the passwords match; otherwise, false.</returns>
     bool Verify(string password, string hashedPassword);
 }
--- Fenicia.Auth/Domains/Security/SecurityService.cs
+++ Fenicia.Auth/Domains/Security/SecurityService.cs
@@ -4,8 +4,18 @@ using Fenicia.Common.Localization;
 
 namespace Fenicia.Auth.Domains.Security;
 
+/// <summary>
+/// Represents a service for handling security-related operations, such as password hashing and verification.
+/// </summary>
 public class SecurityService : ISecurityService
 {
+    /// <summary>
+    /// Hashes the provided original string (typically a password) using the BCrypt algorithm.
+    /// </summary>
+    /// <param name="original">The original string to hash.</param>
+    /// <returns>The hashed string.</returns>
+    /// <exception cref="BadRequestException"></exception>
+    /// <exception cref="InvalidOperationException"></exception>
     public string Hash(string original)
     {
         if (string.IsNullOrEmpty(original))
@@ -18,6 +28,12 @@ public class SecurityService : ISecurityService
         return hashed ?? throw new InvalidOperationException(ExceptionMessages.ErrorHashingPassword);
     }
 
+    /// <summary>
+    /// Verifies whether the provided password matches the hashed password.
+    /// </summary>
+    /// <param name="password">The password to verify.</param>
+    /// <param name="hashedPassword">The hashed password to compare against.</param>
+    /// <returns>true if the passwords match; otherwise, false.</returns>
     public bool Verify(string password, string hashedPassword)
     {
         if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(hashedPassword))
